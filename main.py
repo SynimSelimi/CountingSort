bbaf1d1dee7a6b160321236b45e3fb5384e8a40d
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2020-12-17 21:26:53 +0100

    Add python comparison count sorting

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ def gen_array(n):
   random.shuffle(arr)
   return arr
 
+# Apply Comparison Count Sort on arr
+def comparison_count_sort(arr):
+  size = len(arr)
+  sorted_array = [0] * size
+  count = [0] * size
+
+  for i in range(0, size):
+    for j in range(i + 1, size):
+      if(arr[i] < arr[j]):
+        count[j] += 1
+      else:
+        count[i] += 1
+
+  for i in range(0, size - 1):
+    sorted_array[count[i]] = arr[i]
+
+  return sorted_array
+
 # Method that measures speed of the callback execution 
 def measure_time(callback, *args):
   start = datetime.datetime.now()
@@ -18,9 +36,13 @@ def measure_time(callback, *args):
 # Main demo
 def main():
   # Set test scale
-  scale = 10000
-  arr = measure_time(gen_array, scale)
+  scale = 1000
+  arr = gen_array(scale)
+
+  # Test comparison_count_sort speed
   print(arr[0])
+  sarr = measure_time(comparison_count_sort, arr)
+  print(sarr[0])
 
 # Run main demo
 if __name__ == "__main__":
