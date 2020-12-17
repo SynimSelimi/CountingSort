947b5d954e294038c0000805441f32fbfb447f23
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2020-12-17 21:27:34 +0100

    Add scala comparison count sorting

--- main.scala
+++ main.scala
@@ -8,6 +8,23 @@ def generateArray (n: Int) : Array[Int] = {
   Random.shuffle(arr).toArray
 }
 
+// Apply Comparison Count Sort on a
+def comparisonCountSort(a: Array[Int]): Array[Int] = {
+  val size = a.length
+  val sortedArray: Array[Int] = new Array[Int](size)
+  val count: Array[Int] = new Array[Int](size)
+
+  for (i <- 0 to size - 1; j <- i + 1 to size - 1) {
+    if(a(i) < a(j)) count(j) += 1
+    else count(i) += 1
+  }
+  
+  for (i <- 0 to size - 1)
+    sortedArray(count(i)) = a(i)
+
+  sortedArray
+}
+
 // Measure speed of executing block R 
 def time[R](block: => R): R = {
   val t0 = System.nanoTime()
@@ -21,11 +38,15 @@ def time[R](block: => R): R = {
 // Main demo
 def main(): Unit = {
   // Set test scale
-  var scale = 10000
-  var array = time {
-    generateArray(scale);
-  }
+  var scale = 1000
+  var array = generateArray(scale);
+
+  // Test comparisonCountSort speed
   println(array(0))
+  var sortedArray = time {
+    comparisonCountSort(array)
+  }
+  println(sortedArray(0))
 }
 
 // Run main demo
